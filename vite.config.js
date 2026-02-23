773425b6d65f014f47ab9047a6e7913fe4d5b1dd
TheBogdichHD <89939588+TheBogdichHD@users.noreply.github.com>
2026-02-23 21:12:57 +0300

    fix?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-// vite.config.js
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/cg-1/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: "./index.html",
+        cubes: "./cubes.html"
+      }
+    }
+  }
 });
